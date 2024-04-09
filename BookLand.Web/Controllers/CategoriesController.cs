680ca827ec307cdb4a4b4ebbe5b3fae2a4a4aebe
Youssef Adel <eng.youssef.adel.010@gmail.com>
2024-04-09 13:05:52 +0200

    Enhance the category page

--- BookLand.Web/Controllers/CategoriesController.cs
+++ BookLand.Web/Controllers/CategoriesController.cs
@@ -14,7 +14,7 @@ public class CategoriesController : Controller
     public IActionResult Index()
     {
         // TODO: use ViewModel
-        List<Category> categories = _context.Categories.ToList();
+        List<Category> categories = [.. _context.Categories.AsNoTracking()];
         return View(categories);
     }
 
@@ -68,7 +68,23 @@ public class CategoriesController : Controller
         _context.SaveChanges();
 
         return RedirectToAction(nameof(Index));
+    }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult ToggleState(int id)
+    {
+        Category? category = _context.Categories.Find(id);
+
+        if (category is null)
+            return NotFound();
+
+        category.IsDeleted = !category.IsDeleted;
+        category.LastUpdatedOn = DateTime.Now;
+
+        _context.SaveChanges();
+
+        return Ok(category.LastUpdatedOn.ToString());
     }
 
 }
